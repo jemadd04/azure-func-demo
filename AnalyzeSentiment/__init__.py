5d6422a9538fa7bbc94e2b03faac4b22e478703c
James Maddox <james.maddox07@gmail.com>
2020-04-25 15:53:14 -0400

    func declaration

---
+++ AnalyzeSentiment/__init__.py
@@ -0,0 +1,22 @@
+import logging
+
+import azure.functions as func
+from textblob import TextBlob
+import nltk
+import os
+import json
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    try:
+        req_body = req.get_json()
+    except ValueError:
+        pass
+
+    text = req_body['text']
+    blob = TextBlob(text)
+    polarity = blob.sentiment.polarity
+
+    return func.HttpResponse(json.dumps(polarity), mimetype='application/json')
